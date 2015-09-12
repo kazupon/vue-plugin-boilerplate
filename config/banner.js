7c608cf708f75aea3530bbe9652453e774e90381
kazuya kawaguchi <kawakazu80@gmail.com>
2015-09-12 20:25:28 +0900

    Update banner

--- config/banner.js
+++ config/banner.js
@@ -4,5 +4,5 @@ var version = process.env.VERSION || pack.version
 module.exports =
   pack.name + ' v' + version + '\n' +
   '(c) ' + new Date().getFullYear() +
-  ' ' + pack.author + '\n' +
+  ' ' + pack.author.name + '\n' +
   'Released under the ' + pack.license + ' License.'
