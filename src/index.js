166412109b0533b40111f98f9160bf7b108d4491
kazuya kawaguchi <kawakazu80@gmail.com>
2015-12-06 04:21:25 +0900

    feat(version): support plugin version

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
-export default function (Vue, options = {}) {
+function plugin (Vue, options = {}) {
   Vue.prototype.$add = (a, b) => {
     return a + b
   }
 }
+
+plugin.version = '1.1.0'
+
+export default plugin
