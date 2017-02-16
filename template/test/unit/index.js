d29e70ce83bb9ac142cd7ce32d9ea07c1110a0cf
kazuya kawaguchi <kawakazu80@gmail.com>
2017-02-16 20:29:01 +0900

    :zap: improvement(test): more easy unit testing

--- template/test/unit/index.js
+++ template/test/unit/index.js
@@ -1,12 +1,7 @@
-import Vue from 'vue'
-import plugin from '../../src/index'
-
 // import all helpers
 const helpersContext = require.context('../helpers', true)
 helpersContext.keys().forEach(helpersContext)
 
 // require all test files
-const testsContext = require.context('./', true)
+const testsContext = require.context('./', true, /\.test/)
 testsContext.keys().forEach(testsContext)
-
-Vue.use(plugin)
